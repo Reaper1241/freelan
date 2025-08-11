4765c6b72baad984f7ac263482ffd90b39fc1dbb
shoumq <lae345@mail.ru>
2025-08-11 20:19:05 +0300

    font

--- app.vue
+++ app.vue
@@ -7,9 +7,12 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap');
+
 * {
 margin: 0;
 padding: 0;
+font-family: 'Playfair Display', serif !important;
 }
 html, body {
   width: 100%;
